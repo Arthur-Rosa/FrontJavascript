1b6d8b8ee7cb2aee8f3af605b50d6b8f4648dec3
Arthur Rosa <93738072+Arthur-Rosa@users.noreply.github.com>
2024-12-14 17:25:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const form = document.getElementById('cep-form');
+const cepInput = document.getElementById('cep-input');
+const resultado = document.getElementById('resultado');
+
+form.addEventListener('submit', (event) => {
+    // lógica para buscar o cep
+    event.preventDefault(); // evitar o recarregamento da pagia
+
+    const cep = cepInput.value.trim(); // trim -> remove white space 
+    
+    resultado.innerHTML = '<p>Buscando...</p>';
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Erro ao Buscar o CEP");
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.erro) {
+                resultado.innerHTML = '<p style="color: red;">CEP nao</p>';
+            }
+
+            resultado.innerHTML = `
+                <p><strong>CEP: </strong> ${data.cep}</p>
+                <p><strong>Logradouro: </strong> ${data.logradouro}</p>
+                <p><strong>Bairro: </strong> ${data.bairro}</p>
+                <p><strong>Cidade: </strong> ${data.localidade}</p>
+                <p><strong>Estado: </strong> ${data.uf}</p>
+            `;
+        });
+
+
+});
